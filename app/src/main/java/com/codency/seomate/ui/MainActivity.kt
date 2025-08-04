0efea029f5f729da1b10c4f4ecd8d4e2d54d8227
Codency <apexp7472@gmail.com>
2025-08-03 22:59:06 -0700

    seo

---
+++ app/src/main/java/com/codency/seomate/ui/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.codency.seomate.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.codency.seomate.R
+import com.codency.seomate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize NavController safely
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+            ?: return // Handle error case if needed
+
+        navController = navHostFragment.navController
+
+        // Setup bottom navigation
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/codency/seomate/ui/report/ReportFragment.kt
@@ -0,0 +1,27 @@
+package com.codency.seomate.ui.report
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codency.seomate.databinding.FragmentReportBinding
+
+class ReportFragment : Fragment() {
+
+    private var _binding: FragmentReportBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentReportBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
